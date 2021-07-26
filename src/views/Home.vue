e5fd0a18fb342b9792a3c1fc6ae6bfee12730f23
Michał Paweł Cybulski <m.p.cybulski@stud.elka.pw.edu.pl>
2021-07-26 17:25:41 +0200

    inital commit

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-center">
+    <v-col>
+      <v-btn @click="addscoreForPlayerOne" test rounded color="primary" dark>Rounded Button</v-btn>
+      <span>Player1 {{ scoreForPlayerOne }}</span>
+    </v-col>
+    <v-col>
+      <v-btn @click="addscoreForPlayerTwo" test rounded color="primary" dark>Rounded Button</v-btn>    
+      <span>Player2 {{ scoreForPlayerTwo }}</span>
+    </v-col>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        scoreForPlayerOne: 1,
+        scoreForPlayerTwo: 1,
+      }
+  },
+  methods: {
+    addscoreForPlayerOne() {
+      this.scoreForPlayerOne = ++this.scoreForPlayerOne;
+    },
+    addscoreForPlayerTwo() {
+      this.scoreForPlayerTwo = ++this.scoreForPlayerTwo;
+    }
+  }
+}
+</script>
